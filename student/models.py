199d56a13d0b2f9c3e833ce2e4985aec9d4600d7
aswanth <aswanthshajicv@gmail.com>
2026-07-11 18:28:35 +0530

    added student model

--- student/models.py
+++ student/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+COURSE_CHOICES = [
+  ('Python','Python'),
+  ('Mern','Mern'),
+  ('Spring boot','Spring boot'),
+
+]
+
+class Student(models.Model):
+  name = models.CharField(max_length=20)
+  age = models.IntegerField()
+  place = models.CharField(max_length=30)
+  course = models.CharField(
+    max_length=15,
+    choices=COURSE_CHOICES,
+    default = 'Python')
+
+  def __str__(self):
+    return self.name
