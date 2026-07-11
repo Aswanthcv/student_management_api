34e617801fdc17c0808ce653ad98a232caa30267
aswanth <aswanthshajicv@gmail.com>
2026-07-11 19:54:18 +0530

    get and post methods

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
-  
+   path('students/', StudentApiView.as_view(), name='student-create'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import StudentSerializer
+from .models import *
 
 # Create your views here.
+
+class StudentApiView(APIView):
+    def get(self, request):
+        students = Student.objects.all()
+        print(students)
+        serializer = StudentSerializer(students, many=True)
+        print(serializer.data)
+        return Response(serializer.data)
+
+    def post(self, request):
+
+        serializer = StudentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
